809ecd215aebee3c7919565d70a5a3cdaaec2166
ss3589 <41930495+ss3589@users.noreply.github.com>
2018-09-11 14:42:41 +0900

    Initial commit

---
+++ app/src/main/java/com/cnk24/udpclient/MainActivity.java
@@ -0,0 +1,27 @@
+package com.cnk24.udpclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        UDPClientUtil.getInstance().connectUdpAddressAndPort(2222);
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                UDPClientUtil.getInstance().sendMessage("TEST");
+            }
+        });
+
+    }
+}
